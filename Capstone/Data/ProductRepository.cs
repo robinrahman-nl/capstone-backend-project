ba97145e16459e7108a1db2eb9aea0698b215da0
Robin Rahman <robin.rahman.nl@gmail.com>
2026-02-23 00:07:34 +0100

    feat: implement ProductRepository with full product retrieval

---
+++ Capstone/Data/ProductRepository.cs
@@ -0,0 +1,56 @@
+using Capstone.Models;
+using MySqlConnector;
+
+namespace Capstone.Data;
+
+public class ProductRepository
+{
+    public readonly Database _database;
+
+    public ProductRepository(Database database)
+    {
+        this._database = database;
+    }
+
+    // ------------------------------------------------------------------
+    // METHOD: Retrieves full Product objects.
+    // ------------------------------------------------------------------
+
+public List<Product> GetAllProducts()
+    {
+     using var connection = _database.GetConnection();
+     connection.Open();
+
+    string query = @$"
+SELECT 
+    product_id,
+    product_name,
+    description,
+    product_price,
+    quantity_in_stock
+FROM
+    product_catalogue
+ORDER BY
+    product_id;";
+
+    using var command = new MySqlCommand(query, connection);
+    using var reader = command.ExecuteReader();
+
+    List<Product> products = new List<Product>();
+    while (reader.Read())
+    {   
+        int productId = reader.GetInt32("product_id");
+        string productName = reader.GetString("product_name");
+        string description = reader.GetString("description");
+        double productPrice = reader.GetDouble("product_price");
+        int quantityInStock = reader.GetInt32("quantity_in_stock");
+
+        Product product = new Product(productId, productName, description, productPrice, quantityInStock);
+        products.Add(product);
+
+    }
+    return products;
+    }
+
+    
+}
